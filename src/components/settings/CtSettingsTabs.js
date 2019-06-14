1635e299200dafe3f31d042d4237bfb1a96c8b44
Julian Mann <julian.mann@gmail.com>
2019-06-14 00:29:19 -0500

    adds undsaved file

--- src/components/settings/CtSettingsTabs.js
+++ src/components/settings/CtSettingsTabs.js
@@ -89,34 +89,3 @@ function CtSettingsTabs() {
 }
 
 export default CtSettingsTabs;
-
-// <AppBar position="fixed" className={classes.appBar}>
-//   <Toolbar />
-// </AppBar>;
-
-// <Typography variant="h6" className={classes.title}>
-//     Settings
-//   </Typography>
-
-// import React from 'react';
-// import Paper from '@material-ui/core/Paper';
-// import Tabs from '@material-ui/core/Tabs';
-// import Tab from '@material-ui/core/Tab';
-
-// export default function DisabledTabs() {
-//   const [value, setValue] = React.useState(2);
-
-//   function handleChange(event, newValue) {
-//     setValue(newValue);
-//   }
-
-//   return (
-//     <Paper square>
-//       <Tabs value={value} indicatorColor="primary" textColor="primary" onChange={handleChange}>
-//         <Tab label="Active" />
-//         <Tab label="Disabled" disabled />
-//         <Tab label="Active" />
-//       </Tabs>
-//     </Paper>
-//   );
-// }
