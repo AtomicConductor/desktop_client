27f7eec215d30881191b192fff6a32801ec5b962
Julian Mann <julian.mann@gmail.com>
2019-07-26 18:03:37 -0500

    bluer theme

--- src/theme.js
+++ src/theme.js
@@ -1,6 +1,6 @@
-import blueGrey from "@material-ui/core/colors/grey";
+// import blueGrey from "@material-ui/core/colors/grey";
 import red from "@material-ui/core/colors/red";
-import lightGreen from "@material-ui/core/colors/lightGreen";
+// import lightGreen from "@material-ui/core/colors/lightGreen";
 import green from "@material-ui/core/colors/green";
 
 import yellow from "@material-ui/core/colors/yellow";
@@ -8,17 +8,23 @@ import amber from "@material-ui/core/colors/amber";
 import orange from "@material-ui/core/colors/orange";
 
 import { createMuiTheme } from "@material-ui/core/styles";
-import { teal } from "@material-ui/core/colors";
+import {
+  lightBlue,
+  lightGreen,
+  lime,
+  grey,
+  blueGrey
+} from "@material-ui/core/colors";
 
 export default createMuiTheme({
   palette: {
     type: "dark",
-    primary: blueGrey,
-    secondary: green,
+    primary: grey,
+    secondary: lightBlue,
     // secondary: {
-    //   light: "#65a2bb",
-    //   main: "#19759a",
-    //   dark: "#0c5a7a",
+    //   light: "#64A9E9",
+    //   main: "#3B8EDE",
+    //   dark: "#2888E0",
     //   contrastText: "#fff"
     // },
     error: red
