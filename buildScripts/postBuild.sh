b8217fed10a9b0de085cf91f8df9ea2ccc27b7b5
Anas Karkoukli <43547695+alpha-nu@users.noreply.github.com>
2019-09-24 11:54:02 -0300

    Ct 1161 macos menu (#35)
    update mac menu

--- buildScripts/postBuild.sh
+++ buildScripts/postBuild.sh
@@ -6,4 +6,4 @@ rm build/${appName}/macos64/credits.html
 rm build/${appName}/linux64/credits.html
 
 infoPlist=build/${appName}/macos64/${appName}.app/Contents/Info.plist
-xmlstarlet ed --inplace -u "/plist/dict/key[text() = 'CFBundleName' or text() = 'CFBundleDisplayName']/following-sibling::string[1]" -v 'Conductor Desktop' ${infoPlist} 
+xmlstarlet ed --inplace -u "/plist/dict/key[text() = 'CFBundleName' or text() = 'CFBundleDisplayName']/following-sibling::string[1]" -v 'Conductor' ${infoPlist} 
