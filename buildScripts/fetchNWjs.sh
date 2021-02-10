8c82c42a83c557d68afb0010308f840612b7379b
Francois Lebel <francoislebel@gmail.com>
2021-02-10 15:56:49 -0800

    Host nwjs dependency ourselves

--- buildScripts/fetchNWjs.sh
+++ buildScripts/fetchNWjs.sh
@@ -7,11 +7,8 @@ set -e
 
 nwjs_version="$(jq -r .nwBuilder.version  ./package.json)"
 
-# Fetch, unzip, and move
-url=https://dl.nwjs.io/v${nwjs_version}
-
-# Try mirror
-# url=https://cnpmjs.org/mirrors/nwjs/v${nwjs_version}
+# Fetch, unzip, and move. Use our mirror copy because the official repository is unreliable.
+url=https://downloads.conductortech.com/dependencies
 
 win_folder=nwjs-v${nwjs_version}-win-x64
 mac_folder=nwjs-v${nwjs_version}-osx-x64
@@ -22,17 +19,16 @@ mac_url=${url}/${mac_folder}.zip
 lin_url=${url}/${lin_folder}.tar.gz
 
 
- 
 echo "Downloading ${win_url}..."
-wget ${win_url} 
+wget ${win_url}
 unzip ${win_folder}.zip
 
 echo "Downloading ${mac_url}..."
-wget ${mac_url} 
+wget ${mac_url}
 unzip ${mac_folder}.zip
 
 echo "Downloading ${lin_url}..."
-wget ${lin_url} 
+wget ${lin_url}
 tar xvf ${lin_folder}.tar.gz
 
 echo "Done downloading, cleaning up"
