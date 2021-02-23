d9148317f70ff9eabce16fd140abe65e679d7ab9
Julian Mann <julian.mann@gmail.com>
2021-02-23 11:19:19 -0500

    Do all installer renaming on deploy.

--- buildScripts/deployArtifacts.sh
+++ buildScripts/deployArtifacts.sh
@@ -5,18 +5,26 @@ set -e
 cd /artifacts/installers
 ls -lrt
 
+# All installers are made available with an identifier version-branch
+# Examples:
+# companion-0.4.1-my_fix-osx-installer.dmg
+# companion-0.4.2-master-osx-installer.dmg
+version="$(jq -r .version ../build/package.json)"
+identifier="${version}-$(echo $CI_BRANCH | tr '/' '-')"
+
+mv companion*dmg companion-${identifier}-osx-installer.dmg
+mv companion*exe companion-${identifier}-windows-installer.exe
+mv companion*run companion-${identifier}-linux-installer.run
 aws s3 cp ./ s3://${AWS_S3_BUCKET_NAME}/companion --recursive --acl public-read
 
-# Upload build as `latest` version.
+# If master, copy the same files, renamed to latest.
 if [ "$CI_BRANCH" == "master" ]; then
-    version="$(jq -r .version ../build/package.json)"
-    echo "$version @ `date`" > current-version.txt
 
-    # Update the latest version.
     mv companion*dmg companion-latest-osx-installer.dmg
     mv companion*exe companion-latest-windows-installer.exe
     mv companion*run companion-latest-linux-installer.run
     aws s3 cp companion-latest-* / s3://${AWS_S3_BUCKET_NAME}/companion --recursive --acl public-read
 
+    echo "$version @ `date`" > current-version.txt
     aws s3 cp current-version.txt s3://${AWS_S3_BUCKET_NAME}/companion/current-version.txt --acl public-read
 fi
--- buildScripts/makeInstallers.sh
+++ buildScripts/makeInstallers.sh
@@ -7,18 +7,10 @@ platform=$1
 
 version="$(jq -r .version  /artifacts/build/package.json)"
 
-# We don't number the filename for master. In this way, we don't have to keep
-# changing the download links in docs and main site.
-
-if [ "$CI_BRANCH" == "master" ]; then
-    version=latest
-else
-    version="${version}-dev-$(echo $CI_BRANCH | tr '/' '-')"
-fi
-
 # Delete leftovers from prior runs.
 rm -f /artifacts/installers/companion*
 
+# Build: Override the version and destination
 builder build ./installer/companion.xml ${platform} --setvars project.version=${version} project.outputDirectory=/artifacts/installers --license /opt/license.xml
 
 # builder makes both dmg and app, but we don't want the app because it is seen as a folder in aws.
