76c50ed5272a0dd3017b542fbd05a4bf12f3aa6f
Julian Mann <julian.mann@gmail.com>
2021-02-26 12:04:17 -0500

    Fix bug when deploying the master branch

--- buildScripts/deployArtifacts.sh
+++ buildScripts/deployArtifacts.sh
@@ -23,7 +23,10 @@ if [ "$CI_BRANCH" == "master" ]; then
     mv companion*dmg companion-latest-osx-installer.dmg
     mv companion*exe companion-latest-windows-installer.exe
     mv companion*run companion-latest-linux-installer.run
-    aws s3 cp companion-latest-* / s3://${AWS_S3_BUCKET_NAME}/companion --recursive --acl public-read
+
+    aws s3 cp companion-latest-osx-installer.dmg        s3://${AWS_S3_BUCKET_NAME}/companion/companion-latest-osx-installer.dmg --acl public-read
+    aws s3 cp companion-latest-windows-installer.exe    s3://${AWS_S3_BUCKET_NAME}/companion/companion-latest-windows-installer.exe --acl public-read
+    aws s3 cp companion-latest-linux-installer.run      s3://${AWS_S3_BUCKET_NAME}/companion/companion-latest-linux-installer.run --acl public-read
 
     # Place current-version.txt alongside the build so we can reference it in the docs.
     echo "$version @ `date`" > current-version.txt
