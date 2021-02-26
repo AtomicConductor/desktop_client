e61e997f00322b5632dc3230ce6cefb5391234a1
Julian Mann <julian.mann@gmail.com>
2021-02-26 12:38:03 -0500

    Master deploy bug 2 (#194)
    * Host nwjs dependency ourselves
    
    * Fix latest name
    
    * Ignore missing files
    
    * test
    
    * KIS: replace rename+regex with mv
    
    * Do all installer renaming on deploy.
    
    * comments
    
    * clear installers dir once before installer builds
    
    * cp direct to aws
    
    Co-authored-by: Francois Lebel <francoislebel@gmail.com>

--- buildScripts/deployArtifacts.sh
+++ buildScripts/deployArtifacts.sh
@@ -20,10 +20,9 @@ aws s3 cp ./ s3://${AWS_S3_BUCKET_NAME}/companion --recursive --acl public-read
 # If on master, copy the files, with identifier latest.
 if [ "$CI_BRANCH" == "master" ]; then
 
-    mv companion*dmg companion-latest-osx-installer.dmg
-    mv companion*exe companion-latest-windows-installer.exe
-    mv companion*run companion-latest-linux-installer.run
-    aws s3 cp companion-latest-* / s3://${AWS_S3_BUCKET_NAME}/companion --recursive --acl public-read
+    aws s3 cp companion*dmg    s3://${AWS_S3_BUCKET_NAME}/companion/companion-latest-osx-installer.dmg --acl public-read
+    aws s3 cp companion*exe    s3://${AWS_S3_BUCKET_NAME}/companion/companion-latest-windows-installer.exe --acl public-read
+    aws s3 cp companion*run    s3://${AWS_S3_BUCKET_NAME}/companion/companion-latest-linux-installer.run --acl public-read
 
     # Place current-version.txt alongside the build so we can reference it in the docs.
     echo "$version @ `date`" > current-version.txt
